a65f42705a5b47ece124f22ac4c23ba08e1a49b0
jsguerra <jose@gcostudios.com>
2021-04-17 18:22:02 +0100

    updated script

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+// Set initial state to an empty oboject
 const state = {}
 
+// Setup variables for the frontend
 const display = document.getElementById('display')
 const nameInput = document.getElementById('name')
 const set = document.getElementById('set')
 
-Object.defineProperty(state, "name", {
-  set:(val) => {
-    display.textContent = val
-  }
-})
-
-nameInput.addEventListener('keyup', function() {
-  state.name = this.value
+// Add event listener to change the state property on click
+set.addEventListener('click', () => {
+  state.name = nameInput.value
+  console.log(state.name);
+  display.textContent = state.name
 })
